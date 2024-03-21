2916eef5f8d96099146687691a366df450ec042b
danikova <kovacs.daniel.9509@gmail.com>
2024-03-21 20:55:56 +0100

    limit pictures size to 1mb

---
+++ backend/pb_migrations/1711041753_updated_pictures.js
@@ -0,0 +1,68 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("in1fyqiezqpgivh")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "ccss040m",
+    "name": "file",
+    "type": "file",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "mimeTypes": [
+        "image/jpeg",
+        "image/png",
+        "image/svg+xml",
+        "image/gif",
+        "image/webp"
+      ],
+      "thumbs": [
+        "200x200",
+        "300x300",
+        "1000x1000"
+      ],
+      "maxSelect": 1,
+      "maxSize": 1000000,
+      "protected": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("in1fyqiezqpgivh")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "ccss040m",
+    "name": "file",
+    "type": "file",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "mimeTypes": [
+        "image/jpeg",
+        "image/png",
+        "image/svg+xml",
+        "image/gif",
+        "image/webp"
+      ],
+      "thumbs": [
+        "200x200",
+        "300x300",
+        "1000x1000"
+      ],
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "protected": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
